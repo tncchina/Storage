6545abd2fcc49f3d36e9366172c11f32bbd45ef9
Meiqiu Wang <4742751+mqwang@users.noreply.github.com>
2017-11-26 14:58:24 -0800

    Initial ApiApp setup

---
+++ src/ApiApp.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ApiApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static HttpClient client = new HttpClient();
+
+        Action<string> LogInfo = Console.WriteLine;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //client.BaseAddress = new Uri("http://apiapptest20171126015849.azurewebsites.net/");
+            client.BaseAddress = new Uri("http://localhost:63513/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        [TestMethod]
+        public async Task TestGetAnimalImage()
+        {
+            string content = "";
+            HttpResponseMessage response = null;
+            try
+            {
+                response = await client.GetAsync("api/animalimages/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    content = await response.Content.ReadAsStringAsync();
+                }
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+            catch (Exception e)
+            {
+                LogInfo(e.Message);
+            }
+            finally
+            {
+                LogInfo($"received response status {response.StatusCode}, response content: {content}");
+            }
+        }
+    }
+}
