8a913a97f9cbaab83acc57bbcef8630411e258e1
Svyatoslav Trukhanov <33796852+trukhanov@users.noreply.github.com>
2017-12-09 19:53:57 -0800

    Add BatchUploadClient

---
+++ src/BatchUploadCiient/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BatchUploadClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(594, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Resume upload";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.DetectUrls = false;
+            this.richTextBox.Location = new System.Drawing.Point(24, 27);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.Size = new System.Drawing.Size(550, 540);
+            this.richTextBox.TabIndex = 3;
+            this.richTextBox.Text = "";
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 579);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "BatchUploadClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ src/BatchUploadCiient/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace BatchUploadClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string containerName = "upload";
+            string uploadFolder = folderBrowserDialog.ShowDialog() == DialogResult.OK ? folderBrowserDialog.SelectedPath : null;
+            if (String.IsNullOrWhiteSpace(uploadFolder))
+            {
+                return;
+            }
+
+            string sasToken = CreateContainerSas(containerName);
+
+            richTextBox.Text = richTextBox.Text + $"Obtained SAS from server: {sasToken}" + Environment.NewLine;
+
+            StorageCredentials accountSAS = new StorageCredentials(sasToken);
+
+            CloudStorageAccount accountWithSAS = new CloudStorageAccount(accountSAS, "photostemp", endpointSuffix: null, useHttps: true);
+            CloudBlobClient blobClientWithSAS = accountWithSAS.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClientWithSAS.GetContainerReference(containerName);
+            richTextBox.Text = richTextBox.Text + "List of items in blob:" + Environment.NewLine;
+            foreach (var blob in container.ListBlobs())
+            {
+                richTextBox.Text = richTextBox.Text + blob.Uri.AbsoluteUri + Environment.NewLine;
+            }
+
+            richTextBox.Text = richTextBox.Text + $"Uploading from {uploadFolder}" + Environment.NewLine;
+
+            int truncateLength = uploadFolder.Length + 1;
+            int sucessCount = 0, failureCount = 0;
+
+            foreach (var dirName in Directory.EnumerateDirectories(uploadFolder))
+            {
+                richTextBox.Text = richTextBox.Text + $"Dir: {dirName}" + Environment.NewLine;
+                foreach (var fileName in Directory.EnumerateFiles(dirName))
+                {
+                    string blobName = fileName.Substring(truncateLength).ToLowerInvariant();
+                    CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+                    richTextBox.AppendText($"{fileName} => {blobName} . . . ");
+                    try
+                    {
+                        blob.UploadFromFile(fileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        failureCount++;
+                        richTextBox.Text = richTextBox.Text + "failed." + Environment.NewLine + ex.ToString() + Environment.NewLine;
+                        continue;
+                    }
+
+                    sucessCount++;
+                    richTextBox.AppendText("done." + Environment.NewLine);
+                }
+            }
+
+            richTextBox.Text = richTextBox.Text + $"Batch upload complete. Sucess: {sucessCount}, failure: {failureCount}" + Environment.NewLine + Environment.NewLine;
+        }
+
+        private static string CreateContainerSas(string containerName)
+        {
+            const string ConnectionString = "";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container.GetSharedAccessSignature(new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddDays(7),
+                Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List
+            });
+        }
+
+
+    }
+}
